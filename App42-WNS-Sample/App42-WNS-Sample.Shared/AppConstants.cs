dae8ecb91a2798569f18894eb33dd3bb680abfa0
SamitaMShephertz <samita.mahajan@shephertz.co.in>
2016-03-08 15:57:49 +0530

    Sample added
    Signed-off-by: SamitaMShephertz <samita.mahajan@shephertz.co.in>

--- App42-WNS-Sample/App42-WNS-Sample.Shared/AppConstants.cs
+++ App42-WNS-Sample/App42-WNS-Sample.Shared/AppConstants.cs
@@ -6,14 +6,14 @@ namespace App42_WNS_Sample
 {
     class AppConstants
     {
-        public const String apiKey = "APP_API_KEY"; // Apikey from AppHQ console.
-        public const String secretKey = "APP_SECRET_KEY"; // SecretKey from AppHQ console.
-        public const String userName = "UserName"; // UserName for which device has to be regestered.
+        public const String apiKey = "ef443f34a49a58abb742e4b432c3c216bb200e9018c86412010f84636cf2f25e"; // Apikey from AppHQ console.
+        public const String secretKey = "b51f6363d2811ffdc01b03eb787fee5551d3d33cf6601f9d943f4761d14ae206"; // SecretKey from AppHQ console.
+        public const String userName = "WNSDESKTOP"; // UserName for which device has to be regestered.
         public const String toastTitle = "WNS Toast title";
         public const String toastContent = "WNS Toast content";
         public const String tileTitle = "WNS Tile title";
         public const String tileContent = "WNS Tile content";
-        public const String tileBGImage = "Tile_Background_Image";
+        public const String tileBGImage = "http://www.neb.com/products/~/media/Catalog/All-Products/5CC1360C956A4FA6B3F44D69D10BA74D/Gel%20Photos/N0552_thumb.gif";
         public const String badgeCount = "2";
         public const String tileId = "tileId";
         public const String channelUriKey = "_channelUri"; // No need to change it.
